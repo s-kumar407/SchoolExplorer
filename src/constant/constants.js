4b8e82f7a2bedba7e54fd9f206b29d6ee9a332bf
s-kumar407 <sachinkr407@gmail.com>
2024-03-24 15:52:47 +0530

    fifth commit

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,2 +1,2 @@
-export const base_url=process.env.NEXT_PUBLIC_VERCEL_URL || "https://localhost:";;
-export const port=process.env.NEXT_PUBLIC_PORT ||3000;
+export const base_url=process.env.VERCEL_URL || "https://localhost:";
+export const port=process.env.PORT ||3000;
