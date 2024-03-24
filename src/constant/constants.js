731790324e49fa7a3842d4d7087674541c26b6d1
s-kumar407 <sachinkr407@gmail.com>
2024-03-24 11:59:12 +0530

    second commit

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,2 +1,2 @@
-export const base_url=process.env.NEXT_PUBLIC_BASE_URL;
-export const port=process.env.NEXT_PUBLIC_PORT;
+export const base_url=process.env.NEXT_PUBLIC_BASE_URL || `https://${process.env.VERCEL_URL}`;;
+export const port=process.env.NEXT_PUBLIC_PORT ||3000;
