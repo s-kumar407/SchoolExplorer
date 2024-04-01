c45a3f98db351b0466f6ac0e33aa3be1d8e4262b
s-kumar407 <sachinkr407@gmail.com>
2024-04-01 16:44:41 +0530

    thirteenth commit

--- src/app/schoolProfile/page.js
+++ src/app/schoolProfile/page.js
@@ -12,7 +12,7 @@ import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { base_url, port } from "@/constant/constants";
+import { base_url} from "@/constant/constants";
 
 export default function Component() {
   let [isEditing, setIsEditing] = useState(false);
@@ -35,7 +35,7 @@ export default function Component() {
     localUserData = JSON.parse(localUserData);
 
     let userData = await fetch(
-      `${base_url + port}/api/users/${localUserData.email}`,
+      `${base_url}/api/users/${localUserData.email}`,
       {
         cache: "no-cache",
       }
