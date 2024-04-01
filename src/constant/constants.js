97eadfa174ae7903b8381a93aa89baf8b144d219
s-kumar407 <sachinkr407@gmail.com>
2024-04-01 11:36:39 +0530

    ningth commit

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,3 +1,3 @@
 export const base_url = process.env.VERCEL_ENV === 'production'
-  ? `https://${process.env.VERCEL_URL}`
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : `http://localhost:${process.env.PORT || 3000}`;
